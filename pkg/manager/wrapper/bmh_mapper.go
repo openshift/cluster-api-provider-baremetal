d31d5df11ee2b7a8147fe5f983723e450c6ef2f6
Dmitry Tantsur <dtantsur@protonmail.com>
2024-05-29 13:02:01 +0200

    Fix managerWrapper to use the newer Kind

--- pkg/manager/wrapper/bmh_mapper.go
+++ pkg/manager/wrapper/bmh_mapper.go
@@ -1,6 +1,8 @@
 package wrapper
 
 import (
+	"context"
+
 	bmh "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
 	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
 	"k8s.io/apimachinery/pkg/types"
@@ -10,7 +12,7 @@ import (
 
 // Map will return a reconcile request for a Machine if the event is for a
 // BareMetalHost and that BareMetalHost references a Machine.
-func bmhMap(obj client.Object) []reconcile.Request {
+func bmhMap(_ context.Context, obj client.Object) []reconcile.Request {
 	if host, ok := obj.(*bmh.BareMetalHost); ok {
 		if host.Spec.ConsumerRef != nil && host.Spec.ConsumerRef.Kind == "Machine" && host.Spec.ConsumerRef.APIVersion == machinev1beta1.SchemeGroupVersion.String() {
 			return []reconcile.Request{
