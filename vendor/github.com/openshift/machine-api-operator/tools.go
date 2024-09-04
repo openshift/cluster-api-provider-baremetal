33e0b3ec04056cc26ffa714ade9292e1702fbdee
Theo Barber-Bany <tbarberb@redhat.com>
2024-09-04 16:12:02 +0100

    Updates MAO

--- vendor/github.com/openshift/machine-api-operator/tools.go
+++ vendor/github.com/openshift/machine-api-operator/tools.go
@@ -1,7 +1,12 @@
+//go:build tools
+// +build tools
+
 package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/machine/v1beta1/zz_generated.crd-manifests"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 )
